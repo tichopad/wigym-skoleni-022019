0060d0816050041421a6464f336cf7c4a8a433e8
Michael Tichopád <michael.tichopad@moravio.com>
2019-02-21 21:03:53 +0100

    Zacatecni sablona

---
+++ index.php
@@ -0,0 +1,47 @@
+
+<!DOCTYPE html>
+    <head>
+        <meta charset="utf-8">
+        <title>Registrace a přihlášení</title>
+    </head>
+    <body>
+
+        <div>
+
+            <h1>Registrace</h1>
+
+            <form name="registration" action="" method="post">
+
+                <input type="email" name="email" placeholder="E-mail" required>
+                <br>
+
+                <input type="password" name="password" placeholder="Heslo" required>
+                <br>
+
+                <button type="submit" name="submit">Registrovat</button>
+
+            </form>
+
+        </div>
+
+        <div>
+
+            <h1>Přihlášení</h1>
+
+            <form name="login" action="" method="post">
+
+                <input type="email" name="email" placeholder="E-mail" required>
+                <br>
+
+                <input type="password" name="password" placeholder="Heslo" required>
+                <br>
+
+                <button type="submit" name="submit">Přihlásit</button>
+
+            </form>
+
+        </div>
+
+
+    </body>
+</html>
